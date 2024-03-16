193c5016d9d4a1e4e2f877fe5113e9b37dad3285
abhigully <abhikumar09100@gmail.com>
2024-03-16 23:57:41 +0530

    inital

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
+import NotesArea from './components/NotesArea/NotesArea';
+import { useState } from 'react';
 
 function App() {
+  const [selectedGroupName, setSelectedGroupName] = useState('');
+  const [selectedGroupColor, setSelectedGroupColor] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Sidebar
+        setSelectedGroupName={setSelectedGroupName}
+        setSelectedGroupColor={setSelectedGroupColor}
+      />
+      <NotesArea
+        selectedGroupName={selectedGroupName}
+        selectedGroupColor={selectedGroupColor}
+      />
     </div>
   );
 }
